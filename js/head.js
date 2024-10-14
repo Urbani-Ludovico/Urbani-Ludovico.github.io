52fc3b62d2da461f79f79ffac6f222ae8a015169
Ludovico Urbani <lu.urbani@outlook.com>
2024-10-14 21:35:35 +0200

    Header

--- js/head.js
+++ js/head.js
@@ -21,3 +21,36 @@ function headInit({
 
     head.innerHTML = content;
 }
+
+function headerInit() {
+    const header = document.getElementsByTagName("header")[0];
+
+    const profile = document.createElement("img");
+    profile.src = "/static/profile.jpg";
+    profile.classList.add("profile");
+    header.appendChild(profile);
+
+    const content = document.createElement("div");
+    content.classList.add("content");
+    header.appendChild(content);
+
+    const name = document.createElement("h1");
+    name.innerText = "Ludovico Urbani";
+    name.classList.add("name");
+    content.appendChild(name);
+
+    const definition = document.createElement("span");
+    definition.innerText = "Eterno studente!";
+    definition.classList.add("definition");
+    content.appendChild(definition);
+
+    const birth_date = document.createElement("span");
+    birth_date.innerText = "2 giugno 2003";
+    birth_date.classList.add("other");
+    content.appendChild(birth_date);
+
+    const place = document.createElement("span");
+    place.innerText = "Trieste (IT)";
+    place.classList.add("other");
+    content.appendChild(place);
+}
