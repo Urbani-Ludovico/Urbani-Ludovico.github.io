99c131fdaafd03b34fafcd04e6b014e6c988335c
Ludovico Urbani <lu.urbani@outlook.com>
2024-10-14 21:46:19 +0200

    Page icon

--- js/head.js
+++ js/head.js
@@ -7,7 +7,8 @@ function headInit({
     let content = "<meta charset=\"UTF-8\">" +
         "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\" />" +
         "<link rel=\"stylesheet\" type=\"text/css\" href=\"/styles/base.css\">" +
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/styles/std.css\">";
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/styles/std.css\">" +
+        "<link rel=\"icon\" href=\"/static/icon.svg\">";
 
     if (!!title) {
         content += "<title>" + title + " | Urbani Ludovico - Curriculum</title>"
