1cf765e9faaa806d14cfa1112bfd13ae90497005
Ludovico Urbani <lu.urbani@outlook.com>
2024-10-17 19:35:50 +0200

    Judo titles

---
+++ js/places.js
@@ -0,0 +1,15 @@
+
+const places = {
+    sports_hall: {
+        grado: "Palazzetto dello Sport, Piazzale Azzurri d'Italia 1 - Grado (IT)",
+        palamichieletto: "Palamichieletto, Via Cartiera Vecchia 58 - Sacile (IT)",
+        palapellicone: "Palapellicone, Via dei Sandolini 79 - Ostia Lido (IT)"
+    },
+    dojo_trieste: "ASD DOJO Trieste, Via dei Mille 18 - Trieste (IT)"
+}
+
+const entities = {
+    dojo_trieste: "Dojo Trieste",
+    fijlkam: "Fijlkam",
+    fijlkam_fvg: "Comitato Regionale Fijlkam FVG"
+}
