e3df380c251939723b756fc1e174585c7b304f0b
Ludovico Urbani <lu.urbani@outlook.com>
2024-10-18 08:46:22 +0200

    Shiai competitions

--- js/places.js
+++ js/places.js
@@ -1,34 +1,69 @@
 const places = {
+    hungary: {
+        gyor: {
+            olympic_park: "Olympic Sports Park - Radnóti Miklós u. 46, Győr (HUN)"
+        }
+    },
     italy: {
+        bergamo: {
+            pala_norda: "Pala Norda - Via A. Pitentino, Bergamo (IT)"
+        },
         bologna: {
             centro_tecnico: "Centro tecnico regionale Fijlkam - Via Lirone 46, Castel Maggiore (IT)"
+        },
+        catania: {
+            palaghiaccio: "Palaghiaccio - Viale Kennedy 8/A, Catania (IT)"
+        },
+        genova: {
+            rds: "RDS Stadium - Via G. Canepa 155, Genova (IT)"
         },
         gorizia: {
             palazzetto_grado: "Palazzetto dello Sport - Piazzale Azzurri d'Italia 1, Grado (IT)"
         },
+        grosseto: {
+            palagolfo: "Palagolfo - Via Raffaello Sanzio 5, Follonica (IT)"
+        },
+        laquila: {
+            finanza: "Palazzetto della guardia di finanaza - Via Fiamme Gialle, L'Aquila (IT)"
+        },
         pordenone: {
             flora: "Palestra Flora - Via Peruzza 10, Pordenone (IT)",
             judo_azzanese: "Judo Azzanese - Via Divisione Julia 11/A, Azzano X (IT)",
+            moneterale: "Palazzetto dello Sport - Monetereale Valcellina (IT)",
             palamichieletto: "Palamichieletto - Via Cartiera Vecchia 58, Sacile (IT)",
+            palazzetto: "Palazzetto dello Sport - Via Fratelli Rosselli 4, Pordenone (IT)",
             palazzo_ragazzoni: "Palazzo Ragazzoni - Viale P. Zanzanaro 2, Sacile (IT)"
         },
         roma: {
             palapellicone: "Palapellicone - Via dei Sandolini 79, Ostia Lido (IT)"
         },
+        trento: {
+            palavis: "Palavis - Via S. Udalrico 15, Lavis (IT)"
+        },
+        treviso: {
+            zoppas: "Zoppas Arena - Viale dello Sport 2, Conegliano (IT)"
+        },
         trieste: {
             dojo_trieste: "ASD DOJO Trieste - Via dei Mille 18, Trieste (IT)",
             coni_fvg: "Coni FVG - Via dei Macelli 5, Trieste (IT)",
+            palachiarbola: "Palachiarbola - Via Visinada 5, Trieste (IT)",
             salone_incanti: "Salone degl Incanti - Rive Nazario Sauro, Trieste (IT)"
         },
         udine: {
+            bella_italia: "Bella Italia Village - Via Centrale 22, Lignano Sabbiadoro (IT)",
             benedetti: "Palazzetto dello sport M. Benedetti - Via A. Marangoni 46, Udine (IT)",
             laipacco: "Palestra E. Cernich - Via Laipacco 96, Udine (IT)",
+            tarcento: "Palzzetto dello Sport - Via Sottocolle Verzan 25, Tarcento (IT)",
             uniud_scienze_motorie: "Facoltà di Scienze Motorie (UniUD) - Gemona del Friuli (IT)"
         }
     },
     slovenia: {
         ljubljana: {
-            dvorana_maksa: "Dvorana Maksa Pečarja - Črnuška cesta 9, Ljubljana (SLO)"
+            dvorana_maksa: "Dvorana Maksa Pečarja - Črnuška cesta 9, Ljubljana (SLO)",
+            dvorana_vismarje: "Dvorana Vižmarje Brod - Na Gaju, Ljubljana (SLO)"
+        },
+        koper: {
+            sports_hall: "Sports hall - Cesta Zore Perello-Godina, Koper (SLO)"
         }
     }
 }
