27be3b73448bb1d5bc7ea5a4bfd554c9c3a649fc
Ludovico Urbani <lu.urbani@outlook.com>
2025-02-24 11:32:00 +0100

    IT to ITA

--- js/places.js
+++ js/places.js
@@ -11,61 +11,61 @@ const places = {
     },
     italy: {
         bergamo: {
-            pala_norda: "Pala Norda - Via A. Pitentino, Bergamo (IT)"
+            pala_norda: "Pala Norda - Via A. Pitentino, Bergamo (ITA)"
         },
         bologna: {
-            centro_tecnico: "Centro tecnico regionale Fijlkam - Via Lirone 46, Castel Maggiore (IT)"
+            centro_tecnico: "Centro tecnico regionale Fijlkam - Via Lirone 46, Castel Maggiore (ITA)"
         },
         catania: {
-            palaghiaccio: "Palaghiaccio - Viale Kennedy 8/A, Catania (IT)"
+            palaghiaccio: "Palaghiaccio - Viale Kennedy 8/A, Catania (ITA)"
         },
         genova: {
-            rds: "RDS Stadium - Via G. Canepa 155, Genova (IT)"
+            rds: "RDS Stadium - Via G. Canepa 155, Genova (ITA)"
         },
         gorizia: {
-            palazzetto_grado: "Palazzetto dello Sport - Piazzale Azzurri d'Italia 1, Grado (IT)"
+            palazzetto_grado: "Palazzetto dello Sport - Piazzale Azzurri d'Italia 1, Grado (ITA)"
         },
         grosseto: {
-            palagolfo: "Palagolfo - Via Raffaello Sanzio 5, Follonica (IT)"
+            palagolfo: "Palagolfo - Via Raffaello Sanzio 5, Follonica (ITA)"
         },
         laquila: {
-            finanza: "Palazzetto della guardia di finanaza - Via Fiamme Gialle, L'Aquila (IT)"
+            finanza: "Palazzetto della guardia di finanaza - Via Fiamme Gialle, L'Aquila (ITA)"
         },
         pordenone: {
             favorita: "Palazzatto dello Sport Favorita - Via degli Abeti 9, Spilimbergo (PN)",
-            flora: "Palestra Flora - Via Peruzza 10, Pordenone (IT)",
-            judo_azzanese: "Judo Azzanese - Via Divisione Julia 11/A, Azzano X (IT)",
-            moneterale: "Palazzetto dello Sport - Monetereale Valcellina (IT)",
-            palamichieletto: "Palamichieletto - Via Cartiera Vecchia 58, Sacile (IT)",
-            palazzetto: "Palazzetto dello Sport - Via Fratelli Rosselli 4, Pordenone (IT)",
-            palazzo_ragazzoni: "Palazzo Ragazzoni - Viale P. Zanzanaro 2, Sacile (IT)"
+            flora: "Palestra Flora - Via Peruzza 10, Pordenone (ITA)",
+            judo_azzanese: "Judo Azzanese - Via Divisione Julia 11/A, Azzano X (ITA)",
+            moneterale: "Palazzetto dello Sport - Monetereale Valcellina (ITA)",
+            palamichieletto: "Palamichieletto - Via Cartiera Vecchia 58, Sacile (ITA)",
+            palazzetto: "Palazzetto dello Sport - Via Fratelli Rosselli 4, Pordenone (ITA)",
+            palazzo_ragazzoni: "Palazzo Ragazzoni - Viale P. Zanzanaro 2, Sacile (ITA)"
         },
         roma: {
-            palapellicone: "Palapellicone - Via dei Sandolini 79, Ostia Lido (IT)"
+            palapellicone: "Palapellicone - Via dei Sandolini 79, Ostia Lido (ITA)"
         },
         trento: {
-            palavis: "Palavis - Via S. Udalrico 15, Lavis (IT)"
+            palavis: "Palavis - Via S. Udalrico 15, Lavis (ITA)"
         },
         treviso: {
-            zoppas: "Zoppas Arena - Viale dello Sport 2, Conegliano (IT)"
+            zoppas: "Zoppas Arena - Viale dello Sport 2, Conegliano (ITA)"
         },
         trieste: {
-            coni_fvg: "Coni FVG - Via dei Macelli 5, Trieste (IT)",
-            dojo_trieste: "ASD DOJO Trieste - Via dei Mille 18, Trieste (IT)",
-            makoto: "ASD Makoto - Via P. Mascagni 3/3, Trieste (IT)",
-            palachiarbola: "Palachiarbola - Via Visinada 5, Trieste (IT)",
-            salone_incanti: "Salone degl Incanti - Rive Nazario Sauro, Trieste (IT)"
+            coni_fvg: "Coni FVG - Via dei Macelli 5, Trieste (ITA)",
+            dojo_trieste: "ASD DOJO Trieste - Via dei Mille 18, Trieste (ITA)",
+            makoto: "ASD Makoto - Via P. Mascagni 3/3, Trieste (ITA)",
+            palachiarbola: "Palachiarbola - Via Visinada 5, Trieste (ITA)",
+            salone_incanti: "Salone degl Incanti - Rive Nazario Sauro, Trieste (ITA)"
         },
         udine: {
-            bella_italia: "Bella Italia Village - Via Centrale 22, Lignano Sabbiadoro (IT)",
-            benedetti: "Palazzetto dello sport M. Benedetti - Via A. Marangoni 46, Udine (IT)",
-            laipacco: "Palestra E. Cernich - Via Laipacco 96, Udine (IT)",
-            latisana: "Palazzatto dello sport - Via G. Bottari 33, Latisana (IT)",
-            tarcento: "Palzzetto dello Sport - Via Sottocolle Verzan 25, Tarcento (IT)",
-            uniud_scienze_motorie: "Facolt&agrave; di Scienze Motorie (UniUD) - Gemona del Friuli (IT)"
+            bella_italia: "Bella Italia Village - Via Centrale 22, Lignano Sabbiadoro (ITA)",
+            benedetti: "Palazzetto dello sport M. Benedetti - Via A. Marangoni 46, Udine (ITA)",
+            laipacco: "Palestra E. Cernich - Via Laipacco 96, Udine (ITA)",
+            latisana: "Palazzatto dello sport - Via G. Bottari 33, Latisana (ITA)",
+            tarcento: "Palzzetto dello Sport - Via Sottocolle Verzan 25, Tarcento (ITA)",
+            uniud_scienze_motorie: "Facolt&agrave; di Scienze Motorie (UniUD) - Gemona del Friuli (ITA)"
         },
         verona: {
-            le_grazie: "Palazzetto le Grazie - Strada le Grazie 16, Verona (IT)"
+            le_grazie: "Palazzetto le Grazie - Strada le Grazie 16, Verona (ITA)"
         }
     },
     slovenia: {
